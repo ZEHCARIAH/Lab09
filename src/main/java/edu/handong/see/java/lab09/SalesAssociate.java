cb6e4811f13a64864321b2d7c2eebb249f14dde8
ZEHCARIAH <21700570@handong.edu>
2018-04-07 13:58:03 +0900

    add class and method

---
+++ src/main/java/edu/handong/see/java/lab09/SalesAssociate.java
@@ -0,0 +1,25 @@
+package edu.handong.see.java.lab09;
+
+public class SalesAssociate {
+	
+		private String name;
+		private double sales;
+
+		public String Getname()
+		{
+			return name;
+		}
+
+		public double Getsales() {
+			return sales;
+		}
+		public void Setname(String name)
+		{
+			this.name=name;
+		}
+		public void Setsales(double sales)
+		{
+			this.sales=sales;
+		}
+	}
+
